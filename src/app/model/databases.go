66a24581dd0b01b8d8b79838bb4503cac1b4458c
M R Murazza <razza@qlapa.com>
2019-01-16 11:32:05 +0700

    fix query

--- src/app/model/databases.go
+++ src/app/model/databases.go
@@ -10,14 +10,14 @@ func InitDatabase() {
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS items (" +
 		"sku varchar(20) primary key, " +
 		"name varchar(50) not null, " +
-		"stock integer not null 0," +
+		"stock integer not null default 0, " +
 		"created_at datetime default current_timestamp);")
 	statement.Exec()
 	statement, _ = database.Prepare("INSERT INTO items (sku, name, stock) VALUES (?, ?, ?)")
 	statement.Exec("SSI-D00791015-LL-BWH", "Zalekia Plain Casual Blouse (L,Broken White)", 154)
 
 	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS restock_orders ( " +
-		"id integer autoincrement, " +
+		"id integer auto increment, " +
 		"invoice_id varchar(20) default '(Hilang)', " +
 		"quantity integer not null," +
 		"price biginteger not null, " +
@@ -27,20 +27,20 @@ func InitDatabase() {
 	statement.Exec()
 
 	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS restock_receptions ( " +
-		"id integer autoincrement, " +
+		"id integer auto increment, " +
 		"restock_order_id integer, " +
 		"quantity integer not null," +
 		"created_at datetime default current_timestamp);")
 	statement.Exec()
 
 	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS purchase_orders ( " +
-		"id integer autoincrement, " +
+		"id integer auto increment, " +
 		"order_id varchar(20), " +
 		"quantity integer not null," +
 		"price biginteger not null, " +
 		"SKU varchar(20) not null, " +
 		"item_name varchar(50) not null, " +
-		"notes text', " +
+		"notes text, " +
 		"created_at datetime default current_timestamp);")
 	statement.Exec()
 }
